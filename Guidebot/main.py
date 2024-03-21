0c7462653f92b982051655b067a61a8a0244267d
Parinaz Shiri <74923437+Parinaz-Shiri@users.noreply.github.com>
2024-03-21 20:17:07 -0230

    Add files via upload

---
+++ Guidebot/main.py
@@ -0,0 +1,18 @@
+import pickle
+
+
+#words = ["'s", 'Awesome', 'age', 'aim', 'advice', 'available', 'bot', 'best', 'condition', 'class', 'category', 'Day','different' ,'facilities', 'Good', 'Goodbye', 'Greetings', 'Have', 'high', 'healthier', 'happier', 'I', 'Is', 'known', 'See', 'Thank', 'Thanks', 'That', 'What', 'a', 'activity', 'am', 'any', 'are', 'asbhk', 'available', 'be', 'by', 'bye', 'call', 'complex', 'class', 'cya', 'day', 'different', 'doe', 'entry',  'extra', 'each', 'facility', 'facilities', 'for', 'from', 'going', 'good', 'gvsd', 'hello', 'help', 'helping', 'hey', 'hi', 'hour', 'how', 'in', 'infrastructure', 'is', 'it', 'interested' , 'later', 'leaving', 'like', 'located', 'location', 'many', 'me', 'month', 'much', 'name', 'of', 'one', 'open', 'option', 'or', 'other',  'provided', 'professional', 'requirement', 'see', 'should', 'structure', 'style', 'sport', 'service', 'take', 'teaching', 'thanks', 'the', 'there', 'this', 'to', 'up', 'what', 'when', 'where', 'who', 'will', 'ya', 'year', 'you', 'your', 'information', 'more', 'extra', 'questions', 'quality', 'check', 'website', 'call', 'our', 'com', 'contact', 'telephone', 'number', 'get', 'want', 'if', 'is','guidbot','range', 'tutor', 'society', 'working hour', 'until', 'every day', 'join', 'membership' , 'recommend', 'gold', 'silver', 'bronze', 'type', 'use', 'pool', 'parking', 'path', 'trail', 'walking', 'freely', 'will', 'discount', 'spa', 'services', 'diet', 'plan', 'nutritionist', 'monthly', 'total', 'cost', 'annually', 'health', 'only', 'use', 'gym', 'including', 'basketball', 'volleyball', 'yoga', 'tennis', 'about', 'each', 'write', 'which', 'saturday', 'on', 'wednesday', 'Ms.smith', 'sunday', 'tuesday', 'Mr.Amini', 'monday', 'friday', 'Ms.Jane', 'thursday', 'Mr.Bakhshi', 'see', 'phone', 'secretary', 'management', 'department', 'Emam Hasan', 'Ashrafi Esfehani', 'pounak', 'tehran', 'iran', 'registration', 'requirement', 'ID-card', 'photo', 'process', 'happy', 'here', 'okay']
+
+#pickle_out = open("words.pkl","wb")
+#pickle.dump(words, pickle_out)
+#pickle_out.close()
+
+#classes = ['location', 'options', 'facilities', 'workingHours', 'listOfMembership', 'goldMembership', 'silverMembership', 'BronzeMembership' , 'sportClasses', 'basketball', 'voleyball', 'yoga', 'tennis', 'moreInformation', 'telephone', 'secretaryNumber', 'managementNumber', 'location', 'registerRequirements', 'goodbye', 'greetings', 'invalid', 'name', 'thanks', 'status']
+
+#pickle_out = open("classes.pkl","wb")
+#pickle.dump(classes, pickle_out)
+#pickle_out.close()
+
+#pickle_inn = open('classes.pkl','rb')
+#password = pickle.load(pickle_inn)
+#print(password)
